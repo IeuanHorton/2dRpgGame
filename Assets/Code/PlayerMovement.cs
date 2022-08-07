35dd4156ba9f2401f35f171c66937d58e947934c
Ieuan <Hortonieuan@gmail.com>
2022-08-07 14:15:30 -0400

    Brought in Unity Project
    Has about atleast a weeks worth of work as I had to restart the repo due to not being able to push to origin

---
+++ Assets/Code/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveSpeed = 0f;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private Animator animator;
+
+    private Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Code/StartBattle.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBattle : MonoBehaviour
+{
+
+    //Half way through this https://www.youtube.com/watch?v=1QfxdUpVh5I
+
+    private float timeBetweenAttack;
+
+    [SerializeField]
+    private float maxTimeBetweenAttack = 0;
+    [SerializeField]
+    private float attackRange = 0;
+
+    [SerializeField]
+    private int damage = 0;//Prob uneeded
+
+    [SerializeField]
+    private Transform attackPos;
+    [SerializeField]
+    private LayerMask whatIsEnemies;//Could be unneeded, but could also collect what enemy was dectected
+
+    // I don't think this way of doing it will work for the game
+    [SerializeField]
+    private float attackRangeX;
+    [SerializeField]
+    private float attackRangeY;
+
+    private void Start()
+    {
+        timeBetweenAttack = maxTimeBetweenAttack;
+    }
+
+    private void Update()
+    {
+        if(timeBetweenAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapBoxAll(attackPos.position ,new Vector2(attackRangeX, attackRangeY), 0, whatIsEnemies);//Collects an array of what is inside the hit zone
+            }
+        }
+        else
+        {
+            timeBetweenAttack -= Time.deltaTime;
+        }
+    }
+
+
+}
