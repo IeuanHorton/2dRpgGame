7fd9ffee5de2ed146eef5b7676646ebc613ca9ab
Ieuan <Hortonieuan@gmail.com>
2022-08-08 22:56:54 -0400

    Camera Follow Script

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10);
+    }
+}
