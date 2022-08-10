7f0bde6de088864e26fc023e4a19b251a609a82e
Ieuan <Hortonieuan@gmail.com>
2022-08-09 23:16:32 -0400

    Started on Inventory
    Added Equipment and item script. Added Sprites for items

---
+++ Assets/Code/Items/Equipment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Equipment")]
+public class Equipment : Item
+{
+    [SerializeField]
+    private int armorModifer = 0;
+    [SerializeField]
+    private int damageModifer = 0;
+
+    private EquipmentSlot equipSlot;
+
+    public override void Use()
+    {
+        base.Use();
+    }
+
+    public EquipmentSlot getEquipmentType()
+    {
+        return equipSlot;
+    }
+}
+
+public enum EquipmentSlot { Head, Chest, Legs, Hand1, Hand2 }
---
+++ Assets/Code/Items/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    new private string name = "New Item";
+    private Sprite icon = null;
+
+    public virtual void Use()
+    {
+
+    }
+}
